e7c0543a3fa980c7c731d54db9c0a09108371936
Mehrdad Hedayati <mehrdad.hedayati@gmail.com>
2025-01-04 16:25:15 +0100

    refactor: Enhance test

--- src/test/kotlin/com/mithredate/functional/ShortenedUrlControllerTest.kt
+++ src/test/kotlin/com/mithredate/functional/ShortenedUrlControllerTest.kt
@@ -4,13 +4,13 @@ import com.mithredate.http.request.ShortenUrlRequest
 import com.mithredate.http.response.JsonApiResponse
 import com.mithredate.http.response.ShortUrlResource
 import com.mithredate.repository.ShortUrlRepository
-import io.micronaut.core.type.Argument
-import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
-import io.micronaut.http.client.HttpClient
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
-import io.micronaut.serde.ObjectMapper
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions.*
@@ -18,22 +18,18 @@ import org.junit.jupiter.api.Test
 
 @MicronautTest(environments = ["integration"])
 class ShortenedUrlControllerTest {
-    @field:Client("/")
     @Inject
-    lateinit var client: HttpClient
+    lateinit var client: ShortenedUrlClient
 
     @Inject
     lateinit var shortUrlRepository: ShortUrlRepository
 
-    @Inject
-    lateinit var objectMapper: ObjectMapper
-
     @Test
     fun `should validate url`() {
-        val request = HttpRequest.POST("/api/v1/shortened-urls", ShortenUrlRequest(""))
+        val request = ShortenUrlRequest("")
         val exception =
             assertThrows(HttpClientResponseException::class.java) {
-                client.toBlocking().exchange(request, JsonApiResponse::class.java)
+                client.store(request)
             }
 
         assertEquals(HttpStatus.BAD_REQUEST.code, exception.code())
@@ -42,18 +38,10 @@ class ShortenedUrlControllerTest {
     @Test
     fun `should create short url`() {
         val longUrl = "https://www.example.com"
-        val request = HttpRequest.POST("/api/v1/shortened-urls", ShortenUrlRequest(longUrl))
-        val response =
-            client
-                .toBlocking()
-                .exchange(request, String::class.java)
+        val request = ShortenUrlRequest(longUrl)
+        val response = client.store(request)
         assertSame(HttpStatus.CREATED, response.status)
-        val data =
-            objectMapper
-                .readValue(
-                    response.body().byteInputStream(),
-                    Argument.of(JsonApiResponse::class.java, ShortUrlResource::class.java),
-                ).data as ShortUrlResource
+        val data = response.body()!!.data
         assertEquals(longUrl, data.attributes.longUrl)
         assertNotNull(data.attributes.shortUri)
 
@@ -66,16 +54,10 @@ class ShortenedUrlControllerTest {
                 it
             } ?: fail("ShortUrl not found in repository")
 
-        val retrievedRequest = HttpRequest.GET<String>("/api/v1/shortened-urls/${existing.shortUri}")
-        val retrievedResponse = client.toBlocking().exchange(retrievedRequest, String::class.java)
+        val retrievedResponse = client.show(existing.shortUri)
 
         assertSame(HttpStatus.OK, retrievedResponse.status)
-        val retrievedData =
-            objectMapper
-                .readValue(
-                    retrievedResponse.body().byteInputStream(),
-                    Argument.of(JsonApiResponse::class.java, ShortUrlResource::class.java),
-                ).data as ShortUrlResource
+        val retrievedData = retrievedResponse.body()!!.data
         assertEquals(existing.longUrl, retrievedData.attributes.longUrl)
         assertEquals(existing.shortUri, retrievedData.attributes.shortUri)
         assertEquals(existing.id.toString(), retrievedData.id)
@@ -83,12 +65,19 @@ class ShortenedUrlControllerTest {
 
     @Test
     fun `should return 404 when url does not exist`() {
-        val request = HttpRequest.GET<String>("/api/v1/shortened-urls/non-existing")
-        val exception =
-            assertThrows(HttpClientResponseException::class.java) {
-                client.toBlocking().exchange(request, JsonApiResponse::class.java)
-            }
+        val actual = client.show("non-existing")
 
-        assertEquals(HttpStatus.NOT_FOUND.code, exception.code())
+        assertEquals(HttpStatus.NOT_FOUND.code, actual.code())
     }
+}
+
+@Client("/api/v1/shortened-urls")
+interface ShortenedUrlClient {
+    @Post("/")
+    fun store(
+        @Body request: ShortenUrlRequest,
+    ): HttpResponse<JsonApiResponse<ShortUrlResource>>
+
+    @Get("/{shortUri}")
+    fun show(shortUri: String): HttpResponse<JsonApiResponse<ShortUrlResource>>
 }
